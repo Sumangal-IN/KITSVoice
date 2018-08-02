bb18bfb303a26c971076af6bcb9f59597f7c9299
mandas04 <mandas04@GBKTFDC013X0001.kits.kfplc.com>
2018-08-02 13:33:20 +0530

    Rounding accuracy

--- kitsvoice/intent_service.py
+++ kitsvoice/intent_service.py
@@ -92,7 +92,7 @@ def classify(sentence):
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
-        return_list.append((classes[r[0]], r[1]))
+        return_list.append((classes[r[0]], str(round(r[1], 4))))
     # return tuple of intent and probability
     return return_list
 
